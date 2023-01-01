a6d70b3ed95213886987b04e091d8d64f6612ca4
Ezeali Daniel Okechukwu <ezealiokechukwu1999@gmail.com>
2023-01-01 12:03:52 -0800

    fetching data for latest recipe from database

--- Backend/controllers/recipeController.js
+++ Backend/controllers/recipeController.js
@@ -3,27 +3,32 @@ const category = require("../models/Category");
 const Recipe = require("../models/Recipe");
 exports.homepage = async (req, res, next) => {
   try {
+    const limitNumber = 5;
+    const categories = await category.find({}).limit(limitNumber);
 
-     const limitNumber = 5
-     const categories = await category.find({}).limit(limitNumber)
-     res.render("index", { title: "Cooking blog - Home",categories });
+    const latest = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
+
+    const food = {latest}
+    res.render("index", { title: "Cooking blog - Home", categories, food });
   } catch (error) {
-     res.status(500).send({message: error.message || "error occured"});
+    res.status(500).send({ message: error.message || "error occured" });
   }
 };
 
+// get category
+
 exports.exploreCategories = async (req, res, next) => {
-   try {
- 
-      const limitNumber = 20
-      const categories = await category.find({}).limit(limitNumber)
-      res.render("categories", { title: "Cooking blog - Categories",categories });
-   } catch (error) {
-      res.status(500).send({message: error.message || "error occured"});
-   }
- };
-
-
+  try {
+    const limitNumber = 20;
+    const categories = await category.find({}).limit(limitNumber);
+    res.render("categories", {
+      title: "Cooking blog - Categories",
+      categories,
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message || "error occured" });
+  }
+};
 
 //  async function insertDymmyCategoryData(){
 //    try {
@@ -35,7 +40,7 @@ exports.exploreCategories = async (req, res, next) => {
 //        {
 //          "name": "American",
 //          "image": "american-food.jpg"
-//        }, 
+//        },
 //       {
 //          "name": "Chinese",
 //          "image": "chinese-food.jpg"
@@ -43,7 +48,7 @@ exports.exploreCategories = async (req, res, next) => {
 //        {
 //          "name": "Mexican",
 //          "image": "mexican-food.jpg"
-//        }, 
+//        },
 //        {
 //          "name": "Indian",
 //          "image": "indian-food.jpg"
@@ -59,41 +64,36 @@ exports.exploreCategories = async (req, res, next) => {
 // }
 //  insertDymmyCategoryData()
 
-
-
-
-
-
-async function insertDymmyRecipeData(){
+async function insertDymmyRecipeData() {
   try {
     await Recipe.insertMany([
-      { 
-        "name": "Recipe Name Goes Here",
-        "description": `Recipe Description Goes Here`,
-        "email": "recipeemail@raddy.co.uk",
-        "ingredients": [
+      {
+        name: "Recipe Name Goes Here",
+        description: `Recipe Description Goes Here`,
+        email: "recipeemail@raddy.co.uk",
+        ingredients: [
           "1 level teaspoon baking powder",
           "1 level teaspoon cayenne pepper",
           "1 level teaspoon hot smoked paprika",
         ],
-        "category": "American", 
-        "image": "southern-friend-chicken.jpg"
+        category: "American",
+        image: "southern-friend-chicken.jpg",
       },
-      { 
-        "name": "Recipe Name Goes Here",
-        "description": `Recipe Description Goes Here`,
-        "email": "recipeemail@raddy.co.uk",
-        "ingredients": [
+      {
+        name: "Recipe Name Goes Here",
+        description: `Recipe Description Goes Here`,
+        email: "recipeemail@raddy.co.uk",
+        ingredients: [
           "1 level teaspoon baking powder",
           "1 level teaspoon cayenne pepper",
           "1 level teaspoon hot smoked paprika",
         ],
-        "category": "American", 
-        "image": "southern-friend-chicken.jpg"
+        category: "American",
+        image: "southern-friend-chicken.jpg",
       },
     ]);
   } catch (error) {
-    console.log('err', + error)
+    console.log("err", +error);
   }
 }
-insertDymmyRecipeData()
+insertDymmyRecipeData();
